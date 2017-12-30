51b30711c1d4ffa7ee3e44dbf106fbe4fe5ba9ff
kavera <augustin.kavera@outlook.fr>
2017-12-30 03:47:27 +0100

    Ajout de la classe Manager pour la partie connexion BDD

---
+++ models/Manager.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace models;
+
+use PDO;
+class Manager
+{
+    protected function dbConnect()
+    {
+        try {
+            $db = new PDO('mysql:host=localhost;dbname=projet_3;charset=utf8','root','Commercy1411');
+            return $db;
+        } catch (\Exception $e) {
+            die('Erreur :' . $e->getMessage());
+        }
+    }
+}
