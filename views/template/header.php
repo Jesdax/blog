c019244dd0b8cc1b68606e9ebc7120b6ae7f9842
kavera <augustin.kavera@outlook.fr>
2017-12-30 17:27:42 +0100

    Intégration script dans la vue du header + mise en commentaire de la section vue par une session administrateur + ajout de fichier Js avec modification de bootstrap JS

--- views/template/header.php
+++ views/template/header.php
@@ -5,20 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="Jean Forteroche">
-        <link rel="icon" href="../public/images/favicon.ico">
+        <link rel="icon" href="../../public/images/favicon.ico">
 
         <title><?= $title ;?></title>
 
         <!-- Bootstrap core CSS -->
-        <link href="../public/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../../public/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Custom styles for this template -->
-        <link href="../public/css/starter.css" rel="stylesheet">
+        <link href="../../public/css/starter.css" rel="stylesheet">
     </head>
     <body>
         <header>
             <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-                <a class="navbar-brand" href="#">Billet simple pour l'Alaska</a>
+                <a class="navbar-brand" href="#">Blog 2.0</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -31,7 +31,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="index.php?action=tickets">Articles</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item offset-10">
                             <a class="nav-link" href="index.php?back=backOfficeView">Administration</a>
                         </li>
                     </ul>
@@ -46,8 +46,7 @@
                 </div>
             </nav>
         </header>
-
-        <section>
+        <!--<section>
             <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
@@ -58,19 +57,28 @@
                     </li>
                 </ul>
             </nav>
-                <ul>
-                    <?php if (isset($_SESSION['administrateur'])) { ;?>
+            <ul>
+                <?php /*if (isset($_SESSION['administrateur'])) { ;*/?>
                     <li class="nav-item">
                         <a href="?disconnect=1" class="btn btn-outline-success my-2 my-sm-0">Se déconnecter</a>
                     </li>
-                    <?php } else {;?>
+                <?php /*} else {;*/?>
                     <li class="nav-item">
                         <a href="index.php?back=backOfficeView" class="btn btn-outline-success my-2 my-sm-0">Se connecter</a>
                     </li>
-                    <?php } ;?>
-                </ul>
-        </section>
+                <?php /*} ;*/?>
+            </ul>
+        </section>-->
 
+
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+
+        <script src="../../public/js/jquery.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="../../public/js/popper.min.js"></script>
+        <script src="../../public/js/bootstrap.min.js"></script>
 
     </body>
 </html>
