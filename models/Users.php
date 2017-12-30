9323bce3b93374e6a56ea432295866b7d590e198
kavera <augustin.kavera@outlook.fr>
2017-12-30 22:09:13 +0100

    Ajout de la classe User

---
+++ models/Users.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace models;
+
+
+class Users
+{
+    private $id;
+    private $login;
+    private $password;
+
+    public function __construct($data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $id = (int) $id;
+        if ($id > 0) {
+            $this->id = $id;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     */
+    public function setLogin($login)
+    {
+        $login = (string) $login;
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+}
