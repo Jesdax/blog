a7441cd5dd99e3ce734c77e9ef01784e9400037c
kavera <augustin.kavera@outlook.fr>
2017-12-30 03:24:27 +0100

    Ajout du fichier models contenant la classe post.php

---
+++ models/post.php
@@ -0,0 +1,87 @@
+<?php
+
+
+namespace models;
+
+
+class post
+{
+    private $id;
+    private $title;
+    private $content;
+    private $postDate;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $id = (int) $id;
+        if ($id > 0) {
+            $this->id = $id;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $title = (string) $title;
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $content = (string) htmlspecialchars_decode($content);
+        $this->content = $content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostDate()
+    {
+        return $this->postDate;
+    }
+
+    /**
+     * @param string $postDate
+     */
+    public function setPostDate($date)
+    {
+        $date = (string) $date;
+        $this->postDate = $date;
+    }
+
+
+
+
+}
