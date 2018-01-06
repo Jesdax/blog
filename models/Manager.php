af3a7beaa60842acdc233172312fad5c11c3b824
kavera <augustin.kavera@outlook.fr>
2018-01-06 15:40:40 +0100

    Suppression du password dans le manager

--- models/Manager.php
+++ models/Manager.php
@@ -12,7 +12,7 @@ class Manager
     protected function dbConnect()
     {
         try {
-            $db = new PDO('mysql:host=localhost;dbname=projet_3;charset=utf8','root','Commercy1411');
+            $db = new PDO('mysql:host=localhost;dbname=projet_3;charset=utf8','root','');
             return $db;
         } catch (\Exception $e) {
             die('Erreur :' . $e->getMessage());
